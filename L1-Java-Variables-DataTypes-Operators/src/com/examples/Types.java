de6e03c55353f1481861dd12244dec2215dc135c
Nursultan Khalilov <nursultan.xalilov@kapitalbank.az>
2023-06-06 00:19:57 +0400

    java data typs and operators

---
+++ L1-Java-Variables-DataTypes-Operators/src/com/examples/Types.java
@@ -0,0 +1,64 @@
+package com.examples;
+
+public class Types {
+
+  public static void main(String[] args) {
+
+    //byte
+    //short
+    //int
+    //long
+    //float
+    //double
+    //char
+    //boolean
+
+    //Byte
+    //Short
+    //Integer
+    //Long
+    //Float
+    //Double
+    //Character
+    //Boolean
+
+    byte b = 4; //-128 / 127
+    System.out.println(b);
+
+    short s =  32; // -32,768 / 32,767
+    System.out.println(s);
+
+    int i = 67638686;
+    System.out.println(i);
+
+    long l = 39934932468L;
+    System.out.println(l);
+
+    float f = 2.5F;
+    System.out.println(f);
+
+    double d = 334.67;
+    System.out.println(d);
+
+    char c = '2';
+    System.out.println(c);
+
+    boolean o = true; // true / false
+    System.out.println(o);
+
+    System.out.println("====================");
+
+    Byte b1 = Byte.MAX_VALUE;
+    System.out.println(b1);
+
+    Long l1 = Long.MIN_VALUE;
+    System.out.println(l1);
+
+    Boolean bo1 = new Boolean(true);
+
+    System.out.println(bo1);
+
+
+  }
+
+}
