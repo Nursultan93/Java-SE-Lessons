a6eab528b283595a0fcd439370ef8ecd16195d35
Your Nameclear <you@example.com>
2023-06-12 20:40:51 +0200

    employee updates

--- L3-Java-Break-Continue-AccessModf-Objects/src/tasksStudentScore/Main.java
+++ L3-Java-Break-Continue-AccessModf-Objects/src/tasksStudentScore/Main.java
@@ -2,13 +2,36 @@ package tasksStudentScore;
 
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student("Huseyn", 25, 97);
+        Employee emp1 = new Employee("Nigar", "Afandiyeva");
+        Employee emp2 = new Employee("Vahid", "Rasulov");
+        Employee emp3 = new Employee("Arzu", "Mammadova");
 
-        Student[] array = new Student[5];
+        Employee[] employee = new Employee[3];
+        employee[0] = emp1;
+        employee[1] = emp2;
+        employee[2] = emp3;
 
-
-        for (int i = 0; i <array.length; i++){
-            array[i] = s1.printInfo();
+        for (Employee e: employee){
+            System.out.println(e.getName() + e.getSurname());
         }
+
+
+//        Student s1 = new Student("Huseyn", 25, 97);
+//        Student s2 = new Student("Eli", 23, 56);
+//        Student s3 = new Student("Arzu", 27, 44);
+//
+//        Student[] array = new Student[3];
+//        array[0] = s1;
+//        array[1] = s2;
+//        array[2] = s3;
+//
+//
+//        for (Student student: array){
+//            student.printInfo();
+//        }
+
+
     }
+
+
 }
