6b77b3430eded6a67144f7a06a0921afd8ab0b64
Nursultan Khalilov <nursultan.xalilov@kapitalbank.az>
2023-06-22 21:36:37 +0400

    java abstract class and interface

--- L6-Java-AbstractClass-Interface/src/mail/Main.java
+++ L6-Java-AbstractClass-Interface/src/mail/Main.java
@@ -1,11 +1,18 @@
 package mail;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 public class Main {
   public static void main(String[] args) {
 
     Notification n = new SmsNotification();
 
     n.send("edoicnewionvcewio");
+
 
   }
 }
